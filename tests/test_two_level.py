cb07dcbbc821efc50145d86fb03dd7eb35e5be0b
Jake Ke <jackieke724@hotmail.com>
2025-11-13 01:51:32 -0800

    [confl] max confl = 12

--- tests/test_two_level.py
+++ tests/test_two_level.py
@@ -170,7 +170,7 @@ if args.timeout_cycles > 0:
     print(f"Solver timeout set to: {args.timeout_cycles} cycles")
 
 # Create the SAT solver component
-solver = sst.Component("solver", "satsolver-confl4.SATSolver-confl4")
+solver = sst.Component("solver", "satsolver-confl12.SATSolver-confl12")
 
 # Define memory addresses for global memory operations
 heap_base_addr          = 0x00000000
@@ -216,10 +216,10 @@ solver.addParams(params)
 # Create the external heap subcomponent
 if args.classic_heap:
     print("Using classic heap implementation")
-    heap = solver.setSubComponent("order_heap", "satsolver-confl4.Heap")
+    heap = solver.setSubComponent("order_heap", "satsolver-confl12.Heap")
 else:
     print("Using pipelined heap implementation")
-    heap = solver.setSubComponent("order_heap", "satsolver-confl4.PipelinedHeap")
+    heap = solver.setSubComponent("order_heap", "satsolver-confl12.PipelinedHeap")
 heap.addParams({
     "verbose" : str(args.verbose),
 })
