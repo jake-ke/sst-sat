8a9c927052cc8945ff325acaa0ab11df73461e4e
Jake Ke <jackieke724@hotmail.com>
2025-11-12 18:42:12 -0800

    [opt2] opt1-lits + 7 para wats

--- src/satsolver.h
+++ src/satsolver.h
@@ -48,8 +48,8 @@ public:
     // SST ELI Registrations
     SST_ELI_REGISTER_COMPONENT(
         SATSolver,
-        "satsolver",
-        "SATSolver",
+        "satsolver-opt2-wats",
+        "SATSolver-opt2-wats",
         SST_ELI_ELEMENT_VERSION(1,0,0),
         "SAT Solver Component",
         COMPONENT_CATEGORY_PROCESSOR
