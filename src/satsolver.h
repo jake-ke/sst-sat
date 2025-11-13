46abd5899af5ced7d7c191e9ec4b89f869165c38
Jake Ke <jackieke724@hotmail.com>
2025-11-12 19:19:15 -0800

    [opt5] opt4-heap + prewatcher and free list

--- src/satsolver.h
+++ src/satsolver.h
@@ -48,8 +48,8 @@ public:
     // SST ELI Registrations
     SST_ELI_REGISTER_COMPONENT(
         SATSolver,
-        "satsolver",
-        "SATSolver",
+        "satsolver-opt5-watchlist",
+        "SATSolver-opt5-watchlist",
         SST_ELI_ELEMENT_VERSION(1,0,0),
         "SAT Solver Component",
         COMPONENT_CATEGORY_PROCESSOR
