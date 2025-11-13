116dc460bdaf9fbae5ba231a505ab1b3a0421a68
Jake Ke <jackieke724@hotmail.com>
2025-11-13 01:47:44 -0800

    [confl] max confl = 4

--- src/satsolver.h
+++ src/satsolver.h
@@ -48,8 +48,8 @@ public:
     // SST ELI Registrations
     SST_ELI_REGISTER_COMPONENT(
         SATSolver,
-        "satsolver-opt-final",
-        "SATSolver-opt-final",
+        "satsolver-confl4",
+        "SATSolver-confl4",
         SST_ELI_ELEMENT_VERSION(1,0,0),
         "SAT Solver Component",
         COMPONENT_CATEGORY_PROCESSOR
--- src/structs.h
+++ src/structs.h
@@ -21,7 +21,7 @@ using coro_t = boost::coroutines2::coroutine<void>;
 
 const int PARA_LITS = 8;  // Number of parallel literals to propagate
 const int PROPAGATORS = 7;  // Number of watchers to propagate
-const int MAX_CONFL = 8;  // Maximum number of learned clauses
+const int MAX_CONFL = 4;  // Maximum number of learned clauses
 const int LEARNERS = 8;  // Number of learners for clause learning
 const int HEAPLANES = 8;  // Number of heap lanes for parallel execution
 const int MINIMIZERS = 4;  // Number of minimizers
