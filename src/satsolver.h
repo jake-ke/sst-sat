bbb28a275e73425d10556b45c2f7d11cccc0b5c0
Jake Ke <jackieke724@hotmail.com>
2026-04-15 15:52:43 -0700

    change name for new test branch

--- src/satsolver.h
+++ src/satsolver.h
@@ -49,8 +49,8 @@ public:
     // SST ELI Registrations
     SST_ELI_REGISTER_COMPONENT(
         SATSolver,
-        "satsolver-opt-final",
-        "SATSolver-opt-final",
+        "satsolver",
+        "SATSolver",
         SST_ELI_ELEMENT_VERSION(1,0,0),
         "SAT Solver Component",
         COMPONENT_CATEGORY_PROCESSOR
